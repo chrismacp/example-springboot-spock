8671bb43ba8b88f3af75ba4470ed234d9e66885a
Chris MacPherson <chris@kombine.co.uk>
2017-03-19 11:59:28 +0000

    Create project
    Initial App class has both JUnit and Groovy integration test.

---
+++ src/main/java/scot/macpherson/starter/springbootgroovy/App.java
@@ -0,0 +1,16 @@
+package scot.macpherson.starter.springbootgroovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application start class.
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
